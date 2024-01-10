164f52c61116170ab1d28684f4c2616baab26e1e
th-release <cth.release@gmail.com>
2024-01-10 13:05:11 +0900

    style: File Integration

--- file.go
+++ file.go
@@ -4,7 +4,32 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
+
+func GetFilePath(file string) string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Join(dir, "public", file)
+}
+
+func CreateDirectoryIfNotExist(filePath string) error {
+	dir := filepath.Dir(GetFilePath("") + filePath) // 파일 경로로부터 디렉토리 경로 추출
+
+	// 디렉토리가 이미 존재하는지 확인
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		// 디렉토리가 존재하지 않으면 생성
+		err := os.MkdirAll(dir, os.ModePerm)
+		if err != nil {
+			// 디렉토리 생성 중 오류 발생
+			return err
+		}
+	}
+
+	return nil
+}
 
 func CreateJsonIfNotExist(filePath string, initialContent interface{}) error {
 	_, err := os.Open(filePath)
