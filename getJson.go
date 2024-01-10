c4d30f96dfca989ab0035295070e9170b886bbe8
th-release <cth.release@gmail.com>
2024-01-10 09:57:56 +0900

    feat: Added JsonToMap, JsonToArray

--- getJson.go
+++ getJson.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-func GetJson(filePath string) (interface{}, error) {
+func GetJsonFile(filePath string) (interface{}, error) {
 	fileContent, err := os.ReadFile(filePath)
 	if err != nil {
 		return nil, err
@@ -14,6 +14,26 @@ func GetJson(filePath string) (interface{}, error) {
 	// JSON 파싱
 	var result interface{}
 	err = json.Unmarshal(fileContent, &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func JsonToMap(data string) (map[string]interface{}, error) {
+	var result map[string]interface{}
+	err := json.Unmarshal([]byte(data), &result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func JsonToArray(data string) ([]interface{}, error) {
+	var result []interface{}
+	err := json.Unmarshal([]byte(data), &result)
 	if err != nil {
 		return nil, err
 	}
